5a4153bc92803902d4ae2cdbd0b447a77d639ea7
gurpreetdastirmind <gurpreetd.astirmind@gmail.com>
2026-05-14 14:12:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -55,6 +55,6 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 // server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
